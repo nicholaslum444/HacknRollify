e82dbc65af18eb29e7b50d633cf61447d0bbfc1a
BenedictSu <Benedict_S_S@u.nus.edu>
2016-01-23 20:50:33 +0800

    update files
    update to what the server is hosting

--- try.php
+++ try.php
@@ -1,9 +1,25 @@
 <?php 
-//$dest = imagecreatefrompng("small.png");
-$dest = imagecreatefrompng();
-echo "died";
-$dest = imagecreatefromjpg("ben.jpg");
-$dest = imagecreatefrompng("filled_logo.png");
+require "ImageResize.php";
+include "ResizeImage.php";
+use \Eventviva\ImageResize;
+
+// read the images from file
+$profilepic = imagecreatefromjpeg("ben.jpg");
+
+$profilepicWidth = imagesx($profilepic);
+$profilepicHeight = imagesy($profilepic);
+
+// create the black image overlay
+$blackoverlay = imagecreate($profilepicWidth, $profilepicHeight);
+imagecolorallocate($blackoverlay, 0, 0, 0);
+
+// then merge the black and profilepic
+imagecopymerge($profilepic, $blackoverlay, 0, 0, 0, 0, $profilepicWidth, $profilepicHeight, 50);
+
+// merge the resized logo
+$logoResized = resizeImage("logo.png", $profilepicWidth, 99999);
+imagecopymerge($profilepic, $logoResized, 0, 0, 0, 0, imagesx($logoResized), imagesy($logoResized), 100);
+
+header("Content-Type: image/png");
+imagepng($profilepic);
 ?>
-<img src="ben.jpg">
-<img src="filled_logo.png">
